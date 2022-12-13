8635363b20a20a7c2ecdce4f6df8638421cd304d
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2022-12-13 08:42:21 -0800

    13th Commit, starting create pin page

--- thatlife_frontend/src/components/Pin.jsx
+++ thatlife_frontend/src/components/Pin.jsx
@@ -47,40 +47,44 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save} }) => {
       <div className="m-2">
          <div onMouseEnter={() => setPostHovered(true)} onMouseLeave={() => setPostHovered(false)} onClick={() => navigate(`/pin-detail/${_id}`)} className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out">
          <img src={urlFor(image).width(250).url()} alt="user post" className="rounded-lg w-full" />
-         {postHovered && (
-            <div className="absolute top-0 w-full h-full flex flex-col justify-between p-2 z-50" style={{ height: "100%" }}>
-               <div className="flex items-center justify-between">
-                  <div className="flex gap-2">
-                     <a href={`${image?.asset?.url}?dl=`} download onClick={(e) => e.stopPropagation()} className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none">
-                        <MdDownloadForOffline className="w-7 h-7" />
-                     </a>
+            {postHovered && (
+               <div className="absolute top-0 w-full h-full flex flex-col justify-between p-2 z-50" style={{ height: "100%" }}>
+                  <div className="flex items-center justify-between">
+                     <div className="flex gap-2">
+                        <a href={`${image?.asset?.url}?dl=`} download onClick={(e) => e.stopPropagation()} className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none">
+                           <MdDownloadForOffline className="w-7 h-7" />
+                        </a>
+                     </div>
+                     {alreadySaved ? (
+                        <button type="button" className="bg-red-500 opacity-75 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">{save?.length} Saved</button>
+                     ) : (
+                        <button type="button" className="bg-red-500 opacity-75 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none" onClick={(e) => {
+                           e.stopPropagation();
+                           savePin(_id);
+                        }}>Save</button>
+                     )}
                   </div>
-                  {alreadySaved ? (
-                     <button type="button" className="bg-red-500 opacity-75 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">{save?.length} Saved</button>
-                  ) : (
-                     <button type="button" className="bg-red-500 opacity-75 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none" onClick={(e) => {
-                        e.stopPropagation();
-                        savePin(_id);
-                     }}>Save</button>
-                  )}
+                  <div className="flex justify-between items-center gap-2 w-full">
+                     {destination && (
+                        <a href={destination} target="_blank" rel="noreferrer" className="bg-white flex items-center gap-2 text-black font-bold px-2 py-1 rounded-full opacity-70 hover:opacity-100 hover:shadow-md">
+                           <BsFillArrowUpRightCircleFill /><span style={{fontSize: '.75rem'}}>{destination.length > 24 ? destination.slice(8, 20) : destination.slice(8)}</span>
+                        </a>
+                     )}
+                     {postedBy?._id === user.sub && (
+                        <button type="button" className="bg-white flex items-center opacity-75 hover:opacity-100 text-dark font-4bold p-2 text-base rounded-3xl hover:shadow-md outline-none" onClick={(e) => {
+                           e.stopPropagation();
+                           deletePin(_id);
+                        }}><AiTwotoneDelete /></button>
+                     )}
+                  </div>
                </div>
-               <div className="flex justify-between items-center gap-2 w-full">
-                  {destination && (
-                     <a href={destination} target="_blank" rel="noreferrer" className="bg-white flex items-center gap-2 text-black font-bold px-2 py-1 rounded-full opacity-70 hover:opacity-100 hover:shadow-md">
-                        <BsFillArrowUpRightCircleFill />{destination.length > 24 ? destination.slice(8, 20) : destination.slice(8)}
-                     </a>
-                  )}
-                  {postedBy?._id === user.sub && (
-                     <button type="button" className="bg-white flex items-center opacity-75 hover:opacity-100 text-dark font-4bold p-2 text-base rounded-3xl hover:shadow-md outline-none" onClick={(e) => {
-                        e.stopPropagation();
-                        deletePin(_id);
-                     }}><AiTwotoneDelete /></button>
-                  )}
-               </div>
-            </div>
-         )}
-      </div>
-
+            )}
+         </div>
+         <Link to={`user-profile/${postedBy?._id}`} className="flex gap-2 mt-2 items-center">
+            <img src={postedBy?.image} alt="user avatar" className="w-8 h-8 rounded-full object-cover" referrerPolicy="no-referrer"/>
+            <p className="font-semibold capitalize">{postedBy?.userName}</p>
+         </Link>
+         {/* at 2:58:31 */}
       </div>
    )
 }
