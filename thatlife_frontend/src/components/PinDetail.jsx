47450bd144c3ac4283e374c87c88f85b56e493bb
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-01-04 15:41:57 -0800

    17th COmmit, more Pin detail work

--- thatlife_frontend/src/components/PinDetail.jsx
+++ thatlife_frontend/src/components/PinDetail.jsx
@@ -42,7 +42,37 @@ const PinDetail = ({ user }) => {
    return (
       <div className="flex xl-flex-row flex-col m-auto bg-white" style={{ maxWidth: '1500px', borderRadius: '32px' }}>
          <div className="flex justify-center items-center md:item-start flex-initial">
-            <img src={pinDetails?.image && urlFor(pinDetails.image).url()} alt="" />
+            <img src={pinDetails?.image && urlFor(pinDetails.image).url()} alt="user-post" className="rounded-t-3xl" />
+         </div>
+         <div className="w-full p-5 flex-1 xl:min-w-620">
+            <div className="flex items-center justify-between">
+               <div className="flex gap-2 items-center">
+                  <a href={`${pinDetails.image?.asset?.url}?dl=`} download onClick={(e) => e.stopPropagation()} className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none">
+                     <MdDownloadForOffline className="w-7 h-7" />
+                  </a>
+               </div>
+               <a href={pinDetails.destination} target="_blank" rel="noreferrer">{pinDetails.destination}</a>
+            </div>
+            <div>
+               <h1 className="text-4xl font-bold break-words mt-3">{pinDetails.title}</h1>
+               <p className="mt-2 ml-2">{pinDetails.about}</p>
+            </div>
+            <Link to={`user-profile/${pinDetails.postedBy?._id}`} className="flex gap-2 mt-7 items-center bg-white rounded-lg">
+               <img src={pinDetails.postedBy?.image} alt="user avatar" className="w-8 h-8 rounded-full object-cover" referrerPolicy="no-referrer"/>
+               <p className="font-semibold capitalize">{pinDetails.postedBy?.userName}</p>
+            </Link>
+            <h2 className="mt-5 text-2xl">Comments:</h2>
+            <div className="max-h-370 overflow-y-auto">
+               {pinDetails?.comments?.map((comment, i) => (
+                  <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={i}>
+                     <img src={comment.postedBy.image} alt="user-profile" className="w-10 h-10 rounded-full cursor-pointer" />
+                     <div className="flex flex-col">
+                        <p className="font-bold">{comment.postedBy.userName}</p>
+                        <p>{comment.comment}</p>
+                     </div>
+                  </div>
+               ))}
+            </div>
          </div>
       </div>
    )
