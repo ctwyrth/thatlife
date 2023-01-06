39bf3a83dc32840127516ee88b4ef1552cb072a8
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-01-06 10:33:13 -0800

    19th Commit, fixing comment posting issues

--- thatlife_frontend/src/components/PinDetail.jsx
+++ thatlife_frontend/src/components/PinDetail.jsx
@@ -32,6 +32,7 @@ const PinDetail = ({ user }) => {
             }])
             .commit()
             .then(() => {
+               console.log("immediately after the commit");
                fetchPinDetails();
                setComment('');
                setAddingComment(false);
@@ -48,10 +49,13 @@ const PinDetail = ({ user }) => {
                setPinDetails(data[0]);
 
                if (data[0]) {
+                  console.log("getting similar pins")
                   query = pinDetailMorePinQuery(data[0]);
-
+                  
                   client.fetch(query)
-                     .then((res) => setPins(res));
+                  .then((res) => {
+                        setPins(res);
+                     });
                }
             })
       }
@@ -64,51 +68,73 @@ const PinDetail = ({ user }) => {
    if (!pinDetails) return <Spinner message="Loading pin..." />
    
    return (
-      <div className="flex xl-flex-row flex-col m-auto bg-white" style={{ maxWidth: '1500px', borderRadius: '32px' }}>
-         <div className="flex justify-center items-center md:item-start flex-initial">
-            <img src={pinDetails?.image && urlFor(pinDetails.image).url()} alt="user-post" className="rounded-t-3xl" />
-         </div>
-         <div className="w-full p-5 flex-1 xl:min-w-620">
-            <div className="flex items-center justify-between">
-               <div className="flex gap-2 items-center">
-                  <a href={`${pinDetails.image?.asset?.url}?dl=`} download onClick={(e) => e.stopPropagation()} className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none">
-                     <MdDownloadForOffline className="w-7 h-7" />
-                  </a>
-               </div>
-               <a href={pinDetails.destination} target="_blank" rel="noreferrer">{pinDetails.destination}</a>
+      // pinned image and posting information
+      <>
+         <div className="flex xl-flex-row flex-col m-auto bg-white" style={{ maxWidth: '1500px', borderRadius: '32px' }}>
+            <div className="flex justify-center items-center md:item-start flex-initial">
+               <img src={pinDetails?.image && urlFor(pinDetails.image).url()} alt="user-post" className="rounded-t-3xl" />
             </div>
-            <div>
-               <h1 className="text-4xl font-bold break-words mt-3">{pinDetails.title}</h1>
-               <p className="mt-2 ml-2">{pinDetails.about}</p>
-            </div>
-            <Link to={`user-profile/${pinDetails.postedBy?._id}`} className="flex gap-2 my-6 items-center bg-white rounded-lg">
-               <img src={pinDetails.postedBy?.image} alt="user avatar" className="w-8 h-8 rounded-full object-cover" referrerPolicy="no-referrer"/>
-               <p className="font-semibold capitalize">{pinDetails.postedBy?.userName}</p>
-            </Link>
-            <hr style={{ width: '90%', margin: '0 auto' }} />
-            <h2 className="mt-5 text-2xl">Comments:</h2>
-            <div className="max-h-370 overflow-y-auto">
-               {pinDetails?.comments?.map((comment, i) => (
-                  <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={i}>
-                     <img src={comment.postedBy.image} alt="user-profile" className="w-10 h-10 rounded-full cursor-pointer" />
-                     <div className="flex flex-col">
-                        <p className="font-bold">{comment.postedBy.userName}</p>
-                        <p>{comment.comment}</p>
-                     </div>
+            <div className="w-full p-5 flex-1 xl:min-w-620">
+         {/* image */}
+               <div className="flex items-center justify-between">
+                  <div className="flex gap-2 items-center">
+                     <a href={`${pinDetails.image?.asset?.url}?dl=`} download onClick={(e) => e.stopPropagation()} className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none">
+                        <MdDownloadForOffline className="w-7 h-7" />
+                     </a>
                   </div>
-               ))}
-            </div>
-            <div className="flex flex-wrap items-center mt-6 gap-3">
-               <Link to={`user-profile/${pinDetails.postedBy?._id}`}>
-                  <img src={pinDetails.postedBy?.image} alt="user avatar" className="w-10 h-10 rounded-full cursor-pointer" referrerPolicy="no-referrer"/>
+                  <a href={pinDetails.destination} target="_blank" rel="noreferrer">{pinDetails.destination}</a>
+               </div>
+               <div>
+                  <h1 className="text-4xl font-bold break-words mt-3">{pinDetails.title}</h1>
+                  <p className="mt-2 ml-2">{pinDetails.about}</p>
+               </div>
+         {/* posted by */}
+               <Link to={`user-profile/${pinDetails.postedBy?._id}`} className="flex gap-2 my-6 items-center bg-white rounded-lg">
+                  <img src={pinDetails.postedBy?.image} alt="user avatar" className="w-8 h-8 rounded-full object-cover" referrerPolicy="no-referrer"/>
+                  <p className="font-semibold capitalize">{pinDetails.postedBy?.userName}</p>
                </Link>
-               <input type="text" className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300" placeholder="Add a comment..." value={comment} onChange={(e) => setComment(e.target.value)} />
-               <button type="button" className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none" onClick={addComment}>
-                  {addingComment ? 'Posting...' : 'Posted!'}
-               </button>
+
+               <hr style={{ width: '95%', margin: '0 auto' }} />
+
+         {/* comments on pin */}
+               <h2 className="mt-5 text-2xl pl-6">Comments:</h2>
+               <div className="max-h-370 overflow-y-auto pl-8 mb-7">
+                  {pinDetails?.comments?.map((comment, i) => (
+                     <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={i}>
+                        <img src={comment.postedBy.image} alt="user-profile" className="w-10 h-10 rounded-full" />
+                        <div className="flex flex-col">
+                           <p className="font-bold">{comment.postedBy.userName}</p>
+                           <p>{comment.comment}</p>
+                        </div>
+                     </div>
+                  ))}
+               </div>
+
+               <hr style={{ width: '95%', margin: '0 auto' }} />
+
+         {/* add a comment from current user */}
+               <div className="flex flex-wrap items-center mt-6 gap-3">
+                  <Link to={`user-profile/${user?._id}`}>
+                     <img src={user?.image} alt="user avatar" className="w-10 h-10 rounded-full cursor-pointer" referrerPolicy="no-referrer"/>
+                  </Link>
+                  <input type="text" className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300" placeholder="Add a comment..." value={comment} onChange={(e) => setComment(e.target.value)} />
+                  <button type="button" className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none" onClick={addComment}>
+                     {addingComment ? 'Posting...' : 'Post'}
+                  </button>
+               </div>
             </div>
          </div>
-      </div>
+
+         {/* related pins */}
+         {pins?.length > 0 ? (
+            <>
+               <h2 className="text-center font-bold text-2xl mt-8 mb-4">More like this:</h2>
+               <MasonryLayout pins={pins} />
+            </>
+         ) : (
+            <Spinner message="Loading more pins..." />
+         )}
+      </>
    )
 }
 
--- thatlife_frontend/src/utils/data.js
+++ thatlife_frontend/src/utils/data.js
@@ -58,52 +58,52 @@ export const feedQuery = `*[_type == 'pin'] | order(_createdAt desc) {
 
 export const categories = [
    {
-      name: 'comics',
+      name: 'Comics',
       image: 'https://m.media-amazon.com/images/I/A1bJOvEboML.jpg',
    },
    {
-      name: 'cosplay',
+      name: 'Cosplay',
       image: 'https://pbs.twimg.com/media/FjDvIAHaMAA2jo3?format=jpg&name=large',
    },
    {
-      name: 'gaming',
+      name: 'Gaming',
       image: 'https://www.etonline.com/sites/default/files/styles/max_970x546/public/images/2022-11/GettyImages-1345967858.jpg?h=119335f7&itok=ZJ59Rj1A',
    },
    {
-      name: 'photography',
+      name: 'Photography',
       image: 'https://i.pinimg.com/236x/72/8c/b4/728cb43f48ca762a75da645c121e5c57.jpg',
    },
    {
-      name: 'programming',
+      name: 'Programming',
       image: 'https://www.codingem.com/wp-content/uploads/2021/10/juanjo-jaramillo-mZnx9429i94-unsplash-scaled.jpg',
    },
    {
-      name: 'food',
+      name: 'Food',
       image: 'https://i.pinimg.com/236x/7d/ef/15/7def15ac734837346dac01fad598fc87.jpg',
    },
    {
-      name: 'nature',
+      name: 'Nature',
       image: 'https://i.pinimg.com/236x/b9/82/d4/b982d49a1edd984c4faef745fd1f8479.jpg',
    },
    {
-      name: 'art',
+      name: 'Art',
       image: 'https://i.pinimg.com/736x/f4/e5/ba/f4e5ba22311039662dd253be33bf5f0e.jpg',
    }, {
-      name: 'travel',
+      name: 'Travel',
       image: 'https://i.pinimg.com/236x/fa/95/98/fa95986f2c408098531ca7cc78aee3a4.jpg',
    },
    {
-      name: 'quotes',
+      name: 'Quotes',
       image: 'https://i.pinimg.com/236x/46/7c/17/467c17277badb00b638f8ec4da89a358.jpg',
    }, {
-      name: 'cats',
+      name: 'Cats',
       image: 'https://i.pinimg.com/236x/6c/3c/52/6c3c529e8dadc7cffc4fddedd4caabe1.jpg',
    }, {
-      name: 'dogs',
+      name: 'Dogs',
       image: 'https://i.pinimg.com/236x/1b/c8/30/1bc83077e363db1a394bf6a64b071e9f.jpg',
    },
    {
-      name: 'others',
+      name: 'Others',
       image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
    },
 ];
